1c1599f4e2f0dc41ff5d8e676edd777a2c821692
Scientist94 <shivam.bhattacharjee94@gmail.com>
2015-07-27 20:13:40 +0530

    end of flask basics

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
+#importing Flask class from flask module
 from flask import Flask
 
+#create application process
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
+#using decorators
 @app.route("/")
 @app.route("/hello")
 
+#define view using function, which returns a string
 def hello_world():
 	return "hello WOrld!!"
 
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+#dynamic route with explicit status code
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "hello, {}".format(name)
+	else:
+		return "404: Not fucking Found", 404
+
+#start server using run() method
 if __name__ == "__main__" :
 	app.run()
 
